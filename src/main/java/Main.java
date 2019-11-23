f51abdb0b6d843ada9fcf0576e160846575b32ec
MangKyu <enendekekd12>
2019-11-23 15:46:43 +0900

    Add Calculator

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println(c.add(10, 20));
+        System.out.println(c.min(20, 10));
+    }
+
+}
